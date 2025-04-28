892e3d5bd240c6a0fb84466bf958a5981d7fdeb4
Lorenzo <lorenzonicotera02@gmail.com>
2025-04-28 21:33:45 +0200

    understood string manipulation almost finished the assembly guide, next push strcopy will be done

--- knowledge_exercises/reverse_string.asm
+++ knowledge_exercises/reverse_string.asm
@@ -5,22 +5,15 @@ section .data
 	EXIT_CODE equ 0
 
 	NEW_LINE db 0xa
-	given_input db "hello world!"
 
 section .bss
 	reversed_str resb 12
+	OUTPUT resb  1
 
 section .text
-	_start
+	global _start
 
 _start:
-		mov rsi, given_input
-		xor rcx, rcx
-		;; cld è un po' sospetta
-		cld
-		;;anche il calcolo con il $
-		mov rdi, $ + 15
-		call calculateStrLength
-		xor rax, rax
-		xor rdi, rdi
-		jmp reverseStr
+	pop rcx
+	cmp rcx, 2
+	jne 
